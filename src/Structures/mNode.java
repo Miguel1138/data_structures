61a66d071215916d57020f10d7e6e41928ad4fe2
Miguel <miguel.sdasilva00@gmail.com>
2024-11-07 15:11:54 -0300

    initial Commit
    Structures learned in udemy course.

---
+++ src/Structures/mNode.java
@@ -0,0 +1,19 @@
+package Structures;
+
+public class mNode {
+        protected int value;
+        protected mNode next;
+
+        mNode(int value) {
+            this.value = value;
+        }
+
+    public int getValue() {
+        return value;
+    }
+
+    public mNode getNext() {
+        return next;
+    }
+
+}
---
+++ src/Structures/mStack.java
@@ -0,0 +1,42 @@
+package Structures;
+
+public class mStack {
+    private mNode top;
+    private int height;
+
+    mStack(int value) {
+        mNode newNode = new mNode(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void push (int value) {
+        mNode newNode = new mNode(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    public mNode pop() {
+        if(height == 0) return null;
+
+        mNode temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+
+        return temp;
+    }
+
+    public mNode getTop() {
+        return top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
